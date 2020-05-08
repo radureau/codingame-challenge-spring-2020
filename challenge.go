2e337b7ef1cd732c0580ffd11586ba96d0481c78
Gabriel Radureau <gradureau@figarocms.fr>
2020-05-08 20:05:50 +0200

    wip

--- challenge.go
+++ challenge.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
+	"strings"
 )
 
 func debug(args ...interface{}) {
@@ -20,15 +22,15 @@ var g *Game
 func main() {
 	g = new(Game).WithStdin(os.Stdin)
 	for {
-		debug(g)
 		g.RefreshState()
 
+		outputs := make([]string, len(g.myPacs))
 		for _, pac := range g.myPacs {
-			pac.Play(
+			outputs[pac.ID] = pac.Play(
 				pac.ThinkAboutAMove(),
 			)
-			fmt.Println(pac)
 		}
+		fmt.Println(strings.Join(outputs, "|"))
 	}
 }
 
@@ -39,41 +41,119 @@ func abs(v int) int {
 	return -v
 }
 
-func (p Pos) Dist(pos Pos) int {
-	return abs(p.col-pos.col) + abs(p.row-pos.row)
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func (p Pos) Dist(other Pos) int {
+	return abs(p.col-other.col) + abs(p.row-other.row)
+}
+
+func (pac *Pac) LastMove() *Move {
+	if pac.lastMove != nil {
+		return pac.lastMove
+	}
+	if g.turn <= 1 {
+		return nil
+	}
+	lastState := g.pastStates[0]
+	for _, p := range lastState.myPacs {
+		if pac.ID == p.ID {
+			pac.lastMove = &p.Move
+			break
+		}
+	}
+	return pac.lastMove
+}
+
+// type Direction Pos
+
+// var (
+// 	Up    = Direction{-1, 0}
+// 	Down  = Direction{1, 0}
+// 	Right = Direction{0, 1}
+// 	Left  = Direction{0, -1}
+// 	Stay  = Direction{}
+// )
+
+func (mv Move) Score() float64 {
+	worth := float64(g.pelletsMap.Get(mv.to).PelletValue)
+	worth *= worth * worth
+	dist := mv.from.Dist(mv.to)
+	worth -= float64(dist * dist)
+	return worth
 }
 
 func (pac *Pac) ThinkAboutAMove() Move {
-	var maxPos Pos
-	var maxWorth = -g.Size()
-	for _, pt := range g.pellets {
-		worth := int(pt.PelletValue)
-		worth *= worth
-		worth -= pac.Dist(pt.Pos)
-		if abs(maxWorth-worth) <= 2 { // get the furthest from the closest opponent
-			var minDist = g.Size()
-			var minOpnt *Pac
-			for _, opnt := range g.opponentPacs {
-				dist := pac.Dist(opnt.Pos)
-				if minDist > dist {
-					minDist = dist
-					minOpnt = opnt
-				}
-			}
-			if minOpnt.Dist(pt.Pos) > minOpnt.Dist(maxPos) {
-				maxWorth = worth
-				maxPos = pt.Pos
-			}
-		} else if maxWorth < worth {
-			maxWorth = worth
-			maxPos = pt.Pos
+	// if g.turn > 1 {
+	// 	mv := pac.LastMove()
+	// 	if mv.from != pac.Pos && mv.Score() > 0 {
+	// 		return *mv
+	// 	}
+	// }
+	var closestOpnt, closestAlly *Pac
+	for _, opnt := range g.opponentPacs {
+		closestOpnt = opnt
+	}
+	// for _, ally := range g.myPacs {
+	minDistAlly := g.Size()
+	for _, ally := range g.pacs {
+		if ally.ID == pac.ID {
+			continue
+		}
+		if dist := ally.Dist(pac.Pos); minDistAlly > dist {
+			closestAlly = ally
+			minDistAlly = dist
 		}
 	}
-	return Move{from: pac.Pos, to: maxPos}
+	_, _ = closestAlly, closestOpnt
+	// dot := Pos{
+	// 	row: g.width / len(g.myPacs) * pac.ID,
+	// 	col: g.height / len(g.myPacs) * pac.ID,
+	// }
+	sorted := g.pelletsMap.Ordered(
+		// ByDescRelativePelletValueTo(pac.Pos),
+		ByDescPelletValue{},
+		// ByRelativeDistTo(pac.Pos),
+		// ByDescRelativeDistTo(closestOpnt.Pos),
+		ByDescRelativeDistTo(closestAlly.Pos),
+		// ByRelativeDistTo(dot),
+		ByPosID{},
+	).Sort()
+	// for i := range sorted.sortedPos[:min(5, sorted.Len())] {
+	// 	pt := sorted.get(i)
+	// 	debug(fmt.Sprintf("%v", pt))
+	// }
+	// debug()
+	mv := Move{from: pac.Pos}
+SynchWitlAllies:
+	for _, to := range sorted.sortedPos {
+		mv.to = *to
+		for _, ally := range g.myPacs {
+			if ally.ID == pac.ID ||
+				ally.Move.to == *to ||
+				ally.LastMove() != nil && ally.LastMove().to == *to {
+				continue SynchWitlAllies
+			}
+			return mv
+		}
+	}
+	return mv
 }
 
-func (pac *Pac) Play(mv Move) {
+func (pac *Pac) Play(mv Move) string {
 	pac.Move = mv
+	return pac.String()
 }
 
 func (g *Game) Pacs() []*Pac {
@@ -92,6 +172,7 @@ type Pac struct {
 	speedTurnsLeft  int // unused in wood leagues
 	abilityCooldown int // unused in wood leagues
 	Move
+	lastMove *Move
 }
 
 func (g Game) String() string {
@@ -104,7 +185,7 @@ func (g Game) String() string {
 
 func (g Game) Runes() [][]rune {
 	runes := g.Grid.Runes()
-	for _, pt := range g.pellets {
+	for _, pt := range g.pelletsMap.pellets {
 		runes[pt.row][pt.col] = pellet.Rune()
 	}
 	return runes
@@ -130,7 +211,7 @@ func (g Grid) Runes() [][]rune {
 }
 
 func (p Pac) String() string {
-	return fmt.Sprintf("MOVE %d %d %d", p.ID, p.Move.to.col, p.Move.to.row)
+	return fmt.Sprintf("MOVE %d %d %d %v", p.ID, p.Move.to.col, p.Move.to.row, p.Pos)
 }
 
 type Move struct {
@@ -161,10 +242,11 @@ func (g *Game) WithStdin(in io.Reader) *Game {
 }
 
 func (g *Game) RefreshState() {
-	if g.turn > 0 {
-		g.pastStates = append(g.pastStates, g.GameState)
+	lastGameState := g.GameState
+	if lastGameState.turn > 0 {
+		g.pastStates = append([]*GameState{&lastGameState}, g.pastStates...)
 	}
-	g.turn++
+	g.GameState = GameState{turn: lastGameState.turn + 1}
 
 	g.Scan()
 	fmt.Sscan(g.Text(), &g.myScore, &g.opponentScore)
@@ -192,17 +274,14 @@ func (g *Game) RefreshState() {
 	g.Scan()
 	fmt.Sscan(g.Text(), &g.visiblePelletCount)
 
-	g.pellets = make(map[Pos]Pellet, g.visiblePelletCount)
-	g.superPellets = make(map[Pos]Pellet, g.visiblePelletCount)
+	g.pelletsMap.Init()
 	for i := 0; i < g.visiblePelletCount; i++ {
 		g.Scan()
 		var pt Pellet
 		fmt.Sscan(g.Text(), &pt.col, &pt.row, &pt.PelletValue)
-		g.pellets[pt.Pos] = pt
-		if pt.PelletValue == superPellet {
-			g.superPellets[pt.Pos] = pt
-		}
+		g.pelletsMap.Add(pt)
 	}
+	g.pelletsMap.SortByID()
 }
 
 type Grid struct {
@@ -215,9 +294,17 @@ func (g Grid) Size() int {
 	return g.width * g.height
 }
 
+func (g Grid) Center() Pos {
+	return Pos{col: g.width / 2, row: g.height / 2}
+}
+
 type Pos struct {
 	row int
 	col int
+}
+
+func (p *Pos) String() string {
+	return fmt.Sprintf("%v", *p)
 }
 
 func (p Pos) ID() int {
@@ -225,9 +312,7 @@ func (p Pos) ID() int {
 }
 
 func PosFromID(id int) Pos {
-	p := Pos{row: id % g.width}
-	p.col = id - p.row
-	return p
+	return Pos{row: id % g.width, col: id / g.width}
 }
 
 type Cell struct {
@@ -290,7 +375,7 @@ type Game struct {
 	*bufio.Scanner
 	Grid
 	GameState
-	pastStates []GameState
+	pastStates []*GameState // from latest to oldest
 }
 
 type GameState struct {
@@ -299,9 +384,226 @@ type GameState struct {
 	opponentScore      int
 	visiblePacCount    int // all your pacs and enemy pacs in sight
 	visiblePelletCount int // all pellets in sight
-	pellets            map[Pos]Pellet
-	superPellets       map[Pos]Pellet
+	pelletsMap         PelletMapper
 	pacs               map[Pos]*Pac
 	myPacs             map[Pos]*Pac
 	opponentPacs       map[Pos]*Pac
+}
+
+func (gs GameState) String() string {
+	return fmt.Sprintf("GameState#%d", gs.turn)
+}
+
+type sortedPos []*Pos
+
+func (positions sortedPos) SortByID() {
+	sort.Slice(positions, func(i, j int) bool {
+		return positions[i].ID() < positions[j].ID()
+	})
+}
+
+type PacMapper struct {
+	pacs map[Pos]*Pac
+	sortedPos
+	sorts []PacComparator
+}
+
+func (pm PacMapper) Ordered(comparators ...PacComparator) *PacMapper {
+	npm := pm.Copy()
+	npm.sorts = comparators
+	return npm
+}
+
+func (pm PacMapper) Sort() *PacMapper {
+	sort.Stable(pm)
+	return &pm
+}
+
+func (pm PacMapper) Less(i, j int) bool {
+	pI, pJ := pm.get(i), pm.get(j)
+	for _, cmp := range pm.sorts { // if i == j then use next comparator
+		if cmp.Less(pI, pJ) {
+			return true
+		} else if cmp.Less(pJ, pI) {
+			return false
+		}
+	}
+	return false
+}
+
+func (pm *PacMapper) Init() *PacMapper {
+	pm.pacs = make(map[Pos]*Pac)
+	return pm
+}
+func (pm *PacMapper) Add(p *Pac) {
+	pm.pacs[p.Pos] = p
+	pm.sortedPos = append(pm.sortedPos, &p.Pos)
+}
+
+func (pm PacMapper) Get(pos Pos) *Pac {
+	return pm.pacs[pos]
+}
+
+func (pm PacMapper) get(i int) *Pac {
+	return pm.pacs[*pm.sortedPos[i]]
+}
+
+func (pm PacMapper) Copy() *PacMapper {
+	sp := make(sortedPos, pm.Len())
+	copy(sp, pm.sortedPos)
+	return &PacMapper{
+		pacs:      pm.pacs,
+		sortedPos: sp,
+	}
+}
+
+func (pm PacMapper) Len() int {
+	return len(pm.pacs)
+}
+
+func (pm PacMapper) Swap(i, j int) {
+	pm.sortedPos[i], pm.sortedPos[j] = pm.sortedPos[j], pm.sortedPos[i]
+}
+
+type PelletMapper struct {
+	pellets map[Pos]Pellet
+	sortedPos
+	sorts []PelletComparator
+}
+
+func (pm PelletMapper) Ordered(comparators ...PelletComparator) *PelletMapper {
+	npm := pm.Copy()
+	npm.sorts = comparators
+	return npm
+}
+
+func (pm PelletMapper) String() string {
+	type v struct {
+		ID int
+		PelletValue
+	}
+	s := make([]v, len(pm.sortedPos))
+	for i, pos := range pm.sortedPos {
+		pt := pm.pellets[*pos]
+		s[i] = v{pt.ID(), pt.PelletValue}
+	}
+	return fmt.Sprintf("%v", s[:min(5, len(pm.sortedPos))])
+}
+
+func (pm PelletMapper) Sort() *PelletMapper {
+	sort.Stable(pm)
+	return &pm
+}
+
+func (pm PelletMapper) Less(i, j int) bool {
+	ptI, ptJ := pm.get(i), pm.get(j)
+	for _, cmp := range pm.sorts { // if i == j then use next comparator
+		if cmp.Less(&ptI, &ptJ) {
+			return true
+		} else if cmp.Less(&ptJ, &ptI) {
+			return false
+		}
+	}
+	return false
+}
+
+func (pm *PelletMapper) Init() *PelletMapper {
+	pm.pellets = make(map[Pos]Pellet, g.visiblePelletCount)
+	return pm
+}
+func (pm *PelletMapper) Add(pt Pellet) {
+	pm.pellets[pt.Pos] = pt
+	pm.sortedPos = append(pm.sortedPos, &pt.Pos)
+}
+
+func (pm PelletMapper) Get(pos Pos) Pellet {
+	return pm.pellets[pos]
+}
+
+func (pm PelletMapper) get(i int) Pellet {
+	return pm.pellets[*pm.sortedPos[i]]
+}
+
+func (pm PelletMapper) Copy() *PelletMapper {
+	sp := make(sortedPos, pm.Len())
+	copy(sp, pm.sortedPos)
+	return &PelletMapper{
+		pellets:   pm.pellets,
+		sortedPos: sp,
+	}
+}
+
+func (pm PelletMapper) Len() int {
+	return len(pm.pellets)
+}
+
+func (pm PelletMapper) Swap(i, j int) {
+	pm.sortedPos[i], pm.sortedPos[j] = pm.sortedPos[j], pm.sortedPos[i]
+}
+
+// ORDERING
+
+type PacComparator interface {
+	Less(pI, pJ *Pac) bool
+}
+
+type ByPacID struct{}
+
+func (pm ByPacID) Less(pI, pJ *Pac) bool {
+	return pI.ID < pJ.ID
+}
+
+// _
+
+type PelletComparator interface {
+	Less(ptI, ptJ *Pellet) bool
+}
+
+type ByDescPelletValue struct{}
+
+func (pm ByDescPelletValue) Less(ptI, ptJ *Pellet) bool {
+	return ptI.PelletValue > ptJ.PelletValue
+}
+
+type byRelativeDistTo struct {
+	Pos
+}
+
+func ByRelativeDistTo(pos Pos) PelletComparator {
+	return byRelativeDistTo{pos}
+}
+
+func (pm byRelativeDistTo) Less(ptI, ptJ *Pellet) bool {
+	return ptI.Dist(pm.Pos) < ptJ.Dist(pm.Pos)
+}
+
+type byDescRelativeDistTo struct {
+	Pos
+}
+
+func ByDescRelativeDistTo(pos Pos) PelletComparator {
+	return byDescRelativeDistTo{pos}
+}
+
+func (pm byDescRelativeDistTo) Less(ptI, ptJ *Pellet) bool {
+	return ptI.Dist(pm.Pos) >= ptJ.Dist(pm.Pos)
+}
+
+type ByPosID struct{}
+
+func (pm ByPosID) Less(ptI, ptJ *Pellet) bool {
+	return ptI.ID() < ptJ.ID()
+}
+
+type byDescRelativePelletValueTo struct {
+	Pos
+}
+
+func ByDescRelativePelletValueTo(pos Pos) PelletComparator {
+	return byDescRelativePelletValueTo{pos}
+}
+
+func (pm byDescRelativePelletValueTo) Less(ptI, ptJ *Pellet) bool {
+	return Move{from: ptI.Pos, to: pm.Pos}.Score() >=
+		Move{from: ptJ.Pos, to: pm.Pos}.Score()
 }
