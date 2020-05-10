b0f6a1453c1f275eb8424ec0e535d95dd179a912
Gabriel Radureau <gradureau@figarocms.fr>
2020-05-10 17:41:09 +0200

    display Game grid

--- challenge.go
+++ challenge.go
@@ -17,6 +17,91 @@ type Game struct {
 	width  int // width: size of the grid
 	height int // height: top left corner is (x=0, y=0)
 	graph  *Graph
+	*GameState
+	pastStates []*GameState // from latest to oldest
+}
+
+func (G *Game) String() string {
+	s := ""
+	for y := 0; y < G.height; y++ {
+		runes := make([]rune, G.width)
+		for x := 0; x < G.width; x++ {
+			if cell, ok := G.graph.cells[xy(x, y)]; ok {
+				r := ' '
+				_ = cell
+				runes[x] = r
+			} else {
+				runes[x] = '#'
+			}
+		}
+		s += string(runes) + "\n"
+	}
+	return s
+}
+
+// GameState _
+type GameState struct {
+	*Game
+	turn
+	myScore            ScorePoint
+	opponentScore      ScorePoint
+	visiblePacCount    int // all your pacs and enemy pacs in sight
+	visiblePelletCount int // all pellets in sight
+}
+
+func (gs *GameState) String() string {
+	return fmt.Sprintf("GameState#%d", gs.turn)
+}
+
+type turn int
+
+// MaxTurn _
+const MaxTurn = turn(200)
+
+// ReadGameState _
+func (G *Game) ReadGameState() {
+	if G.GameState == nil {
+		G.GameState = &GameState{Game: G, turn: 1}
+		G.pastStates = make([]*GameState, 0, MaxTurn-1)
+	} else {
+		G.pastStates = append(G.pastStates, G.GameState)
+		G.GameState = &GameState{Game: G, turn: G.turn + 1}
+	}
+	G.Scan()
+	fmt.Sscan(G.Text(), &G.myScore, &G.opponentScore)
+
+	G.Scan()
+	fmt.Sscan(G.Text(), &G.visiblePacCount)
+
+	for i := 0; i < G.visiblePacCount; i++ {
+		// pacID: pac number (unique within a team)
+		// mine: true if this pac is yours
+		// x: position in the grid
+		// y: position in the grid
+		// typeID: unused in wood leagues
+		// speedTurnsLeft: unused in wood leagues
+		// abilityCooldown: unused in wood leagues
+		var pacID int
+		var mine bool
+		var _mine int
+		var x, y int
+		var typeID string
+		var speedTurnsLeft, abilityCooldown int
+		G.Scan()
+		fmt.Sscan(G.Text(), &pacID, &_mine, &x, &y, &typeID, &speedTurnsLeft, &abilityCooldown)
+		mine = _mine != 0
+		_ = mine
+	}
+
+	G.Scan()
+	fmt.Sscan(G.Text(), &G.visiblePelletCount)
+
+	for i := 0; i < G.visiblePelletCount; i++ {
+		// value: amount of points this pellet is worth
+		var x, y, value int
+		G.Scan()
+		fmt.Sscan(G.Text(), &x, &y, &value)
+	}
 }
 
 func (G *Game) scanWidthAndHeight() {
@@ -180,6 +265,23 @@ func move(from, to *Cell) Move {
 	return Move{from.Pos, to.Pos}
 }
 
+// ScorePoint _
+type ScorePoint int
+
+// Pellets possible score point values
+const (
+	NormalPellet = ScorePoint(1)
+	SuperPellet  = ScorePoint(10)
+)
+
+type freshness int
+
+// Pellet _
+type Pellet struct {
+	value ScorePoint
+	freshness
+}
+
 // Cell _
 type Cell struct {
 	Pos
@@ -209,53 +311,20 @@ func GameFromIoReader(in io.Reader) *Game {
 	return G
 }
 
+func (G *Game) PlayFirstTurn() {
+	G.ReadGameState()
+}
+
 func main() {
 	G = GameFromIoReader(os.Stdin)
 	G.buildGraph()
-	debug(G.graph)
-	os.Exit(0)
+
+	G.PlayFirstTurn()
 	for {
-		var myScore, opponentScore int
-		G.Scan()
-		fmt.Sscan(G.Text(), &myScore, &opponentScore)
-		// visiblePacCount: all your pacs and enemy pacs in sight
-		var visiblePacCount int
-		G.Scan()
-		fmt.Sscan(G.Text(), &visiblePacCount)
-
-		for i := 0; i < visiblePacCount; i++ {
-			// pacID: pac number (unique within a team)
-			// mine: true if this pac is yours
-			// x: position in the grid
-			// y: position in the grid
-			// typeID: unused in wood leagues
-			// speedTurnsLeft: unused in wood leagues
-			// abilityCooldown: unused in wood leagues
-			var pacID int
-			var mine bool
-			var _mine int
-			var x, y int
-			var typeID string
-			var speedTurnsLeft, abilityCooldown int
-			G.Scan()
-			fmt.Sscan(G.Text(), &pacID, &_mine, &x, &y, &typeID, &speedTurnsLeft, &abilityCooldown)
-			mine = _mine != 0
-			_ = mine
-		}
-		// visiblePelletCount: all pellets in sight
-		var visiblePelletCount int
-		G.Scan()
-		fmt.Sscan(G.Text(), &visiblePelletCount)
-
-		for i := 0; i < visiblePelletCount; i++ {
-			// value: amount of points this pellet is worth
-			var x, y, value int
-			G.Scan()
-			fmt.Sscan(G.Text(), &x, &y, &value)
-		}
-
+		break
+		G.ReadGameState()
 		// fmt.Fprintln(os.Stderr, "Debug messages...")
 		fmt.Println("MOVE 0 15 10") // MOVE <pacID> <x> <y>
-		break
 	}
+	fmt.Println(G)
 }
